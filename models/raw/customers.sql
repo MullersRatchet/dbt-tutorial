560f9a948ff183ff78401049f28847b24fff4d46
Tracie Ivy <tracie.ivy@mandsconsulting.com>
2024-07-05 14:14:50 +0000

    curated and edw models

---
+++ models/raw/customers.sql
@@ -0,0 +1,14 @@
+{{ config(materialized='view') }}
+
+with customer as (
+
+select 
+    id as customer_id,
+    first_name,
+    last_name
+from {{ source('jaffle_shop', 'customers') }}
+
+)
+
+select *
+from customer
